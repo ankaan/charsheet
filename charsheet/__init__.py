4f954432f3d0b16e282287900a96a46406b9454e
Anders Engström <ankan@ankan.eu>
2014-05-20 20:39:42 +0200

    Improved default form formatting

--- charsheet/__init__.py
+++ charsheet/__init__.py
@@ -1,13 +1,21 @@
 from pyramid.authentication import AuthTktAuthenticationPolicy
 from pyramid.config import Configurator
+from pyramid.path import AssetResolver
 from pyramid.security import ALL_PERMISSIONS
 
+from deform import Form
+from pkg_resources import resource_filename
 from sqlalchemy import engine_from_config
 
 from .models import Base
 from .models import db
 from .models import get_groups
 from .models import get_root
+
+resolver = AssetResolver()
+
+def resolve(path):
+    return resolver.resolve(path).abspath()
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
@@ -16,6 +24,10 @@ def main(global_config, **settings):
     db.configure(bind=engine)
     Base.metadata.bind = engine
     config = Configurator(settings=settings,root_factory=get_root)
+
+    deform_templates = resource_filename('deform', 'templates')
+    deform_search_path = (resolve('charsheet:templates/deform'), deform_templates)
+    Form.set_zpt_renderer(deform_search_path)
 
     config.include('pyramid_persona')
     config.set_authentication_policy(AuthTktAuthenticationPolicy(
